2b690bead0b3a4a51519a36b23761475bfaafc8d
SaedHossam <30476540+SaedHossam@users.noreply.github.com>
2021-02-11 17:12:16 +0200

    Added Starter Project

---
+++ SecretWordGame/GamePlay.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecretWordGame
+{
+    public partial class GamePlay : Form
+    {
+        private string secretWord;
+        private string difficulty;
+        private string category;
+
+        public string Difficulty
+        {
+            get
+            {
+                return difficulty;
+            }
+            set
+            {
+                difficulty = value;
+                lblDifficulty.Text = difficulty;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+                lblCategory.Text = category;
+            }
+        }
+
+        List<char> pressedKeys;
+        SoundPlayer simpleSound;
+
+        public GamePlay()
+        {
+            InitializeComponent();
+            simpleSound = new SoundPlayer();
+
+            secretWord = "elephant";
+
+            pressedKeys = new List<char>();
+            pressedKeys.Add('A');
+            pressedKeys.Add('V');
+            pressedKeys.Add('R');
+            pressedKeys.Add('M');
+
+            listBox1.DataSource = pressedKeys;
+
+            DrawWord();
+            DrawKeyBoard();
+        }
+
+        private void DrawWord()
+        {
+            FlowLayoutPanel panel = new FlowLayoutPanel();
+
+            Label[] labels = new Label[secretWord.Length];
+            for (int i = 0; i < secretWord.Length; ++i)
+            {
+                labels[i] = new Label();
+                labels[i].Text = "_";
+                //labels[i].Text = SecretWord[i].ToString();
+                labels[i].Font = new Font("Time New Romans", 18);
+                labels[i].Width = 25;
+                labels[i].Height = 30;
+            }
+
+            panel.Controls.AddRange(labels);
+
+            panel.AutoSize = true;
+            panel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            panel.Location = new Point((this.Width - panel.Size.Width) / 2, 50);
+            panel.Anchor = AnchorStyles.Top;
+            panel.Padding = new Padding(10);
+            panel.BackColor = Color.FromArgb(150, Color.White);
+
+            this.Controls.Add(panel);
+        }
+
+        private void DrawKeyBoard()
+        {
+            Button[] buttons = new Button[26];
+            FlowLayoutPanel keyBoardPanel = new FlowLayoutPanel();
+            keyBoardPanel.Name = "keyBoardPanel";
+
+            for (int i = 0; i < 26; ++i)
+            {
+                buttons[i] = new Button();
+                buttons[i].Text = ((char)(i + 'A')).ToString();
+                buttons[i].Width = 65;
+                buttons[i].Height = 40;
+                buttons[i].Font = new Font("Time New Romans", 16);
+                buttons[i].Cursor = Cursors.Hand;
+            }
+            keyBoardPanel.Width = 520;
+            keyBoardPanel.Height = 200;
+            keyBoardPanel.Padding = new Padding(10);
+            keyBoardPanel.Location = new Point((this.Width - keyBoardPanel.Size.Width) / 2, this.Height - 300);
+            keyBoardPanel.Anchor = AnchorStyles.Bottom;
+            //keyBoardPanel.BackColor = Color.FromArgb(100, Color.White);
+
+            keyBoardPanel.Controls.AddRange(buttons);
+            this.Controls.Add(keyBoardPanel);
+        }
+
+        private void EnableKeyBoard(bool enable)
+        {
+            foreach (Control c in this.Controls["keyBoardPanel"].Controls)
+            {
+                if (enable && !pressedKeys.Contains(c.Text[0]))
+                {
+                    c.Enabled = true;
+                }
+                else
+                {
+                    c.Enabled = false;
+                }
+            }
+        }
+
+        private void btnFreeze_Click(object sender, EventArgs e)
+        {
+            EnableKeyBoard(false);
+        }
+
+        private void btnUnfreeze_Click(object sender, EventArgs e)
+        {
+            EnableKeyBoard(true);
+        }
+
+        private void GamePlay_Load(object sender, EventArgs e)
+        {
+            //simpleSound.SoundLocation = @"./410574__yummie__game-background-music-loop-short.wav";
+            simpleSound.SoundLocation = @"./489035__michael-db__game-music-01.wav";
+            simpleSound.PlayLooping();
+
+            btnUnfreeze_Click(null, null);
+        }
+
+        private void btnStopSound_Click(object sender, EventArgs e)
+        {
+            simpleSound.Stop();
+        }
+
+        private void GamePlay_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            simpleSound.Stop();
+        }
+    }
+}
---
+++ SecretWordGame/OptionsDialog.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace SecretWordGame
+{
+    partial class OptionsDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.cbDifficulty = new System.Windows.Forms.ComboBox();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(136, 195);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(100, 35);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(347, 195);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 35);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // cbDifficulty
+            // 
+            this.cbDifficulty.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDifficulty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDifficulty.FormattingEnabled = true;
+            this.cbDifficulty.Location = new System.Drawing.Point(250, 48);
+            this.cbDifficulty.Name = "cbDifficulty";
+            this.cbDifficulty.Size = new System.Drawing.Size(170, 28);
+            this.cbDifficulty.TabIndex = 2;
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(250, 113);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(170, 28);
+            this.cbCategory.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Difficulty";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(163, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Category";
+            // 
+            // OptionsDialog
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(582, 253);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCategory);
+            this.Controls.Add(this.cbDifficulty);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Name = "OptionsDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OptionsDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ComboBox cbDifficulty;
+        private System.Windows.Forms.ComboBox cbCategory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
